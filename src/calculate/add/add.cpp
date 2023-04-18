995e48a4c166a6cf2fa8fdbb9191093752139655
ryoga <surpace2479@gmail.com>
2023-04-19 08:23:58 +0900

    コメントの動作テスト

--- src/calculate/add/add.cpp
+++ src/calculate/add/add.cpp
@@ -1,7 +1,7 @@
 #include <calculate/add/add.hpp>
 
 namespace calculate {
-
+// 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。 足し算をします。
 int Add::get(int a, int b) { return a + b; };
 
 }  // namespace calculate
--- src/calculate/sub/sub.cpp
+++ src/calculate/sub/sub.cpp
@@ -1,7 +1,7 @@
 #include <calculate/sub/sub.hpp>
 
 namespace calculate {
-
+    //   引き算   //
     int Sub::get(int a, int b) { 
         return a - b; 
     };
